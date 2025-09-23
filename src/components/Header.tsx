a0c7040512123e9d518690865319346b9434710e
Amir <am.mohammadi@ravanertebat.com>
2025-09-23 12:09:41 +0330

    Update logo assets in Header component: replace LOGO-header.png, add LOGO-title.png, and remove LOGO-header3.png.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,29 @@
 import LOGOHeader from "@/assets/LOGO-header.png";
+import LOGOTITLE from "@/assets/LOGO-title.png";
 
 const Header = () => {
   return (
-    <header className="text-white py-16" style={{ backgroundColor: "#d2080b" }}>
+    <header
+      className="text-white flex items-center justify-center min-h-screen"
+      style={{ backgroundColor: "#d2080b" }}
+    >
       <div className="container mx-auto px-4 text-center">
         {/* Logo and Title */}
         <div className="flex flex-col items-center mb-6">
           <img
             src={LOGOHeader}
             alt="Bergeerd Logo"
-            className="w-32 h-32 md:w-48 md:h-48 mb-4 drop-shadow-lg hover:scale-110 transition-all duration-300 ease-in-out animate-pulse hover:animate-bounce"
+            className="w-48 h-48 md:w-48 md:h-48 mb-4 drop-shadow-lg hover:scale-110 transition-all duration-300 ease-in-out animate-pulse hover:animate-bounce"
             style={{
               animation:
                 "logoFloat 1s ease-in-out infinite, logoWiggle 1s ease-in-out infinite",
             }}
           />
-          <h1 className="font-diner text-6xl md:text-8xl tracking-wider">
-            Bergeerd{" "}
-          </h1>
+          <img
+            src={LOGOTITLE}
+            alt="Bergeerd LogoTitle"
+            className=" h-48 md:h-48 mb-4 "
+          />
         </div>
 
         <p className="text-xl md:text-2xl font-medium opacity-90 max-w-2xl mx-auto font-persian">
